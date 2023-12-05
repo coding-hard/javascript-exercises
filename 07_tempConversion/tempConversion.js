76ec0e801c31ec49beaddd44283edbee985d89a0
Dmytro Vozniuk <dmytro.vozniuk@revolut.com>
2023-12-05 16:45:08 +0100

    Solve 7th exercise

--- 07_tempConversion/tempConversion.js
+++ 07_tempConversion/tempConversion.js
@@ -1,8 +1,21 @@
-const convertToCelsius = function() {
+const convertToCelsius = function (temperature) {
+  // formula - x °F ≘ (x − 32) × 5/9°C
+
+  let actualTemperature = (temperature - 32) * (5 / 9);
+  return Math.round(actualTemperature * 10) / 10;
+
 };
 
-const convertToFahrenheit = function() {
+// console.log(convertToCelsius(32));
+
+const convertToFahrenheit = function (temperature) {
+  // formula - x °C ≘ (x × 9/5+ 32) °
+
+  let actualTemperature = (temperature * (9 / 5)) + 32;
+  return Math.round(actualTemperature * 10) / 10;
 };
+
+// console.log(convertToFahrenheit(0));
 
 // Do not edit below this line
 module.exports = {
